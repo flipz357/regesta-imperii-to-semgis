c67b7182150ddfc7215dd86c869e87779612c3c6
opitz <opitz@last.cl.uni-heidelberg.de>
2021-08-24 14:03:15 +0200

    delete unecessary files

--- quick_download_geo_names.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget -O geodata/allCountries.zip https://download.geonames.org/export/dump/allCountries.zip 
-echo "Finished download, unzipping..."
-unzip geodata/allCountries.zip -d geodata/
-echo "Finished"
--- quick_download_intermediate_files.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wget -O src/resources/ENTITIES_1.json https://www.cl.uni-heidelberg.de/~opitz/data/ENTITIES_1.json
-wget -O src/resources/CANDIDATES_1.json https://www.cl.uni-heidelberg.de/~opitz/data/CANDIDATES_1.json
-echo "Finished download"
